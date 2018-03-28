38727029be0234318cd09fb02d69869fb6772f24
Avi <aganguli@andrew.cmu.edu>
2018-03-28 11:41:18 -0400

    converted to python2.7

--- python/src/audio.py
+++ python/src/audio.py
@@ -1,44 +1,43 @@
 import os
-from queue import Queue
+from Queue import Queue
 from threading import Thread
 from time import sleep
-from typing import Dict, Callable
 
 import pygame
 
 
 class Audio:
 
-    def play_sound(self, drum: str, volume: int) -> None:
+    def play_sound(self, drum, volume):
         """
         plays audio given drum type from map given above and volume from 0 to 100 using threads
         :param drum: type of drum
         :param volume: volume from 0 to 100
         :return: nothing - plays sound
         """
-        get_dir: Callable[[str], str] = os.path.dirname
-        current: str = os.path.realpath(__file__)
-        directory: str = os.path.join(get_dir(get_dir(get_dir(current))), 'samples')
-        file_name: str = os.path.join(directory, self.__drum_to_file[drum])
-        sound: pygame.mixer.Sound = pygame.mixer.Sound(file_name)
+        get_dir = os.path.dirname
+        current = os.path.realpath(__file__)
+        directory = os.path.join(get_dir(get_dir(get_dir(current))), 'samples')
+        file_name = os.path.join(directory, self.__drum_to_file[drum])
+        sound = pygame.mixer.Sound(file_name)
         sound.set_volume(volume / 100.0)
-        channel: pygame.mixer.Channel = sound.play()
+        channel = sound.play()
         while channel.get_busy():
             sleep(0.0001)
 
-    def __worker(self) -> None:
+    def __worker(self):
         """
         worker plays sound with drum and volume from queue
         :return: None, runs forever
         """
         while True:
-            (args): (str, int) = self.__task_queue.get()
-            drum: str = args[0]
-            volume: int = args[1]
+            (args) = self.__task_queue.get()
+            drum = args[0]
+            volume = args[1]
             self.play_sound(drum, volume)
             self.__task_queue.task_done()
 
-    def __test(self) -> None:
+    def __test(self):
         """
         tests audio
         :return: None
@@ -126,13 +125,13 @@ class Audio:
         self.__task_queue.put(('tom', 100))
         self.__task_queue.join()
 
-    def __init__(self, num_channels: int = 20) -> None:
+    def __init__(self, num_channels = 20):
         """
         constructor that initializes data structures
         :return: None
         """
         # map of drum type to file name
-        self.__drum_to_file: Dict[str, str] = {
+        self.__drum_to_file = {
             'clap': 'clap-analog.wav',
             'cowbell': 'cowbell-808.wav',
             'crash': 'crash-acoustic.wav',
@@ -147,14 +146,14 @@ class Audio:
             'tom': 'tom-acoustic01.wav'
         }
         # Task queue that contains sounds to be played
-        self.__task_queue: Queue = Queue()
+        self.__task_queue = Queue()
         pygame.mixer.init()
         pygame.mixer.set_num_channels(num_channels)
         for i in range(num_channels):
-            t: Thread = Thread(target=self.__worker)
+            t = Thread(target=self.__worker)
             t.setDaemon(True)
             t.start()
         self.__test()
 
 
-test: Audio = Audio()
+test = Audio()
