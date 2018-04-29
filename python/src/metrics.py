51cace8ae0e3a8b0ab1bba4ff03dcf82f9232da4
Avi <aganguli@andrew.cmu.edu>
2018-04-29 11:27:31 -0400

    changed docs

--- python/src/metrics.py
+++ python/src/metrics.py
@@ -1,12 +1,12 @@
-def test_latency(actual_arr, expected_arr):
+def test_latency(initial_time_arr, final_time_arr):
     """
     tests latency of drum hit algorithm
-    :param actual_arr: array of actual values obtained from testing
-    :param expected_arr: array of expected values input by tester
+    :param initial_time_arr: array of initial values obtained from Arduino
+    :param final_time_arr: array of final times input by tester
     :return: average latency
     """
-    total = sum([(actual - expected) for (actual, expected) in zip(actual_arr, expected_arr)])
-    num = len(actual_arr)
+    total = sum([(final - initial) for (final, initial) in zip(initial_time_arr, final_time_arr)])
+    num = len(final_time_arr)
     return float(total/num)
 
 
@@ -17,5 +17,5 @@ def test_accuracy(actual_arr, expected_arr):
     :param expected_arr: array of expected values input by tester
     :return: number of errors in run
     """
-    num_errors = sum([1 for (actual, expected) in zip(actual_arr, expected_arr) if actual != expected])
+    num_errors = sum([1 for (actual, expected) in zip(actual_arr, expected_arr) if (actual != expected)])
     return num_errors
